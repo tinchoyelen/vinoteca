800c75266c54ddb97d45e44a9115bea5b2ce225e
Martin Yelen <yomaryelen_14@hotmail.com>
2023-02-13 21:47:08 -0300

    ultimo commit

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
         <CartWidget/>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">VINOTECA</a>
+        <a class="navbar-brand" href="#"> LA VINOTECA</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
